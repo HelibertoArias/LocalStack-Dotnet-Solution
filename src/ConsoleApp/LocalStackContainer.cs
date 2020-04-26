879db50187f13d7a835a4f71c576fdba2222e90b
helibertoarias <HelibertoArias@gmail.com>
2020-04-25 20:23:49 -0500

    Add project files.

---
+++ src/ConsoleApp/LocalStackContainer.cs
@@ -0,0 +1,125 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class LocalStackContainer : IAsyncDisposable
+    {
+        private readonly DockerClient _dockerClient;
+        private const string _imageName = "localstack/localstack:0.11.0";
+        private string _containerId;
+        private const string _containerName = "localstack-container";
+
+        public LocalStackContainer() => _dockerClient = new DockerClientConfiguration(new Uri(GetDockerUri())).CreateClient();
+
+        public async Task RunContainer()
+        {
+            try
+            {
+                await CleanUpContainer();
+
+                await GetImage();
+
+                await StartContainer();
+            }
+            catch (Exception ex)
+            {
+                await CleanUpContainer();
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private async Task GetImage()
+        {
+            await _dockerClient.Images
+                .CreateImageAsync(new ImagesCreateParameters
+                {
+                    FromImage = _imageName
+                },
+                new AuthConfig(),
+                new Progress<JSONMessage>());
+        }
+
+        private async Task StartContainer()
+        {
+            var response = await _dockerClient.Containers.CreateContainerAsync(new CreateContainerParameters
+            {
+                Name = _containerName,
+                Image = _imageName,
+                ExposedPorts = new Dictionary<string, EmptyStruct>
+                {
+                    { "4572", default},
+                    { "8081", default},
+                },
+
+                HostConfig = new HostConfig
+                {
+                    PortBindings = new Dictionary<string, IList<PortBinding>>
+                    {
+                        {"4572", new List<PortBinding> {new PortBinding { HostPort = "4572" } }},
+                        {"8081", new List<PortBinding> {new PortBinding { HostPort = "8081" } }}
+                    }
+                },
+                Env = new List<string> { "SERVICES=s3:4572", "PORT_WEB_UI=8081" }
+            });
+
+            _containerId = response.ID;
+            Console.WriteLine("Starting up the localstack container");
+            await _dockerClient.Containers.StartContainerAsync(_containerId, null);
+            Console.WriteLine("LocalStack is running");
+        }
+
+        private async Task CleanUpContainer()
+        {
+            var containertToDelete = (await _dockerClient.Containers
+                                        .ListContainersAsync(new ContainersListParameters { All = true }))
+                                        .Where(x => x.Names.Any(n => n.Contains(_containerName)));
+
+            if (containertToDelete.Any())
+            {
+                foreach (var item in containertToDelete)
+                {
+                    if (item.State.Equals("running"))
+                        await _dockerClient.Containers.KillContainerAsync(item.ID, new ContainerKillParameters());
+                    else
+                        await _dockerClient.Containers.StopContainerAsync(item.ID, new ContainerStopParameters());
+
+                    await _dockerClient.Containers.RemoveContainerAsync(item.ID, new ContainerRemoveParameters { Force = true });
+                }
+            }
+        }
+
+        private string GetDockerUri()
+        {
+            var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+            if (isWindows)
+            {
+                return "npipe://./pipe/docker_engine";
+            }
+
+            var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+            if (isLinux)
+            {
+                return "unix:/var/run/docker.sock";
+            }
+
+            throw new Exception("Unable to determine what OS this is running on");
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_containerId != null)
+            {
+                Console.WriteLine("Shutdown localstack container");
+                await CleanUpContainer();
+            }
+        }
+    }
+}
---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await RunS3Example();
+        }
+
+        private static async Task RunS3Example()
+        {
+            await using (var stacklocal = new LocalStackContainer())
+            {
+                await stacklocal.RunContainer();
+
+                var config = new AmazonS3Config()
+                {
+                    ServiceURL = "http://localhost:4572/",
+                    ForcePathStyle = true,
+                    Timeout = TimeSpan.FromSeconds(5)
+                };
+                var credentials = new BasicAWSCredentials("tem", "temp");
+
+                IAmazonS3 s3Client = new AmazonS3Client(credentials, config);
+
+                var bucketName = "bucket-test";
+
+                Console.WriteLine($"Creating bucket {bucketName}");
+                await s3Client.PutBucketAsync(bucketName);
+
+                var list = await s3Client.ListBucketsAsync();
+
+                Console.WriteLine("Reading buckets...");
+                list.Buckets.ForEach((bucket) => { Console.WriteLine(bucket.BucketName); });
+
+                const string path = @"dummy-file.jpg";
+
+                var file = File.Create(path);
+
+                Console.WriteLine("Uploading a file..");
+                await s3Client.UploadObjectFromStreamAsync(bucketName, path, file, null);
+
+                Console.WriteLine($"List files in bucket {bucketName}");
+                var filesResponse = await s3Client.ListObjectsAsync(bucketName);
+                filesResponse.S3Objects.ForEach((s3Object) => Console.WriteLine(s3Object.Key));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
